08bc79fda4edeab800de23d3d0cc82b1274fc1ff
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-11-21 15:04:19 +0900

    code cov

--- jest.config.ts
+++ jest.config.ts
@@ -16,6 +16,9 @@ const config: Config = {
     // ...
     "^@/components/(.*)$": "<rootDir>/components/$1",
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage", // 생성된 리포트를 저장할 디렉터리
+  coverageReporters: ["json", "lcov", "text", "clover"], // 다양한 형식으로 리포트 생성
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
